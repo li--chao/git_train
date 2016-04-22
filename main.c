2a216e39b0d853c7278f5cc3fe7e40f7d11be701
li--chao <tianguodexiaoge@hotmail.com>
2016-04-22 21:24:36 +0800

    to trigger jenkins

--- main.c
+++ main.c
@@ -17,5 +17,6 @@ int main(const int argc, const char** argv)
     printf("master commit\n");
     printf("will check to master\n");
     printf("second version\n");
+    printf("second version\n")
     return 0;
 }
